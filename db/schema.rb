8685faf82919ec89a87174d1b7fd14564e372f6f
tubakurou <89577938+tubakurou@users.noreply.github.com>
2022-01-19 08:27:34 +0000

    uodate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_061249) do
+ActiveRecord::Schema.define(version: 2022_01_19_082507) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -73,6 +73,29 @@ ActiveRecord::Schema.define(version: 2022_01_19_061249) do
     t.integer "price"
     t.boolean "is_active"
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "order_id"
+    t.integer "price"
+    t.integer "making_status"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "shipping_cost"
+    t.integer "payment_method"
+    t.integer "total_payment"
+    t.string "name"
+    t.string "adress"
+    t.string "postal_code"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
