0c988a4bc28d362a844001b9bfa8b193c0edf86e
tubakurou <89577938+tubakurou@users.noreply.github.com>
2022-01-19 06:18:39 +0000

    update db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  
+  devise_for :admins
+  devise_for :customers, controllers: {
+  registrations: "customer/registrations",
+  sessions: 'customer/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
