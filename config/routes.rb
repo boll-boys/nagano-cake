d334b95fbfd245192aa3b2bcb93a57b13747b68a
wazuo <95359918+wazuo@users.noreply.github.com>
2022-01-20 05:11:32 +0000

    making top

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  
+
   devise_for :admins
   devise_for :customers, controllers: {
   registrations: "customer/registrations",
   sessions: 'customer/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+
 end
