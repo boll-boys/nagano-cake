31de41cb20284f973462b05120d075a08866be96
tubakurou <89577938+tubakurou@users.noreply.github.com>
2022-01-20 05:12:43 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+    :last_name,:fist_name,:last_name_kana,:fist_name_kana,:postal_code,:address,:phon_number,:email
+    ])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  
-  devise_for :admins
-  devise_for :customers, controllers: {
-  registrations: "customer/registrations",
-  sessions: 'customer/sessions'
+
+  get 'homes/top'
+  devise_for :admin,skip: [:registrations, :passwords]  ,controllers: {
+  sessions: 'adomin/sessions'
+  }
+
+  devise_for :customers,skip: [:passwords,] ,controllers: {
+  registrations: "customers/registrations",
+  sessions: 'customers/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
