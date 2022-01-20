60768519afc1ac5ac25d2139092f1f1f085d34b1
tubakurou <89577938+tubakurou@users.noreply.github.com>
2022-01-20 12:17:36 +0000

    update devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  get "/about" => "homes#about" 
   root to: 'homes#top'
   devise_for :admin,skip: [:registrations, :passwords]  ,controllers: {
   sessions: 'adomin/sessions'
