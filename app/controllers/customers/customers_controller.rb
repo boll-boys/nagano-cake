e58e67d8c15ab3c3681d6c159ac4aececbab7aab
tubakurou <89577938+tubakurou@users.noreply.github.com>
2022-01-22 08:25:13 +0000

    index

---
+++ app/controllers/customers/customers_controller.rb
@@ -0,0 +1,25 @@
+class Customers::CustomersController < ApplicationController
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+   if  @customer.update(user_params)
+       flash[:notice] = "have updated user successfully."
+    redirect_to user_path(@customer.id)
+else
+    render :edit
+    end
+end
+private
+  
+  
+   def user_params
+    params.require(:customer).permit(:fist_name, :, :introduction)
+  end
+end
