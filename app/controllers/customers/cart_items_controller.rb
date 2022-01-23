b35dc3f0a29f6c81e685a43f005566c1be7bb700
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-01-23 07:55:06 +0000

    Add cart

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,11 +1,13 @@
 class Customers::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items.all
+    @total_price= @cart_items.sum { |cart_item| cart_item.item.price * cart_item.amount }
+
   end
 
 	def update
     @cart_item.update(amount: params[:cart_item][:amount].to_i)
-    @price = sub_price(@cart_item).to_s(:delimited)
+    @price = sub_total(@cart_item).to_s(:delimited)
     @cart_items = current_cart
     @total = amount(@cart_items).to_s(:delimited)
     # redirect_to customers_cart_items_path
@@ -13,8 +15,9 @@ class Customers::CartItemsController < ApplicationController
 
 	def create
     @cart_item = CartItem.new(params_cart_item)
-     @cart_item.save
-      redirect_to cart_items_path
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
 
 	end
 
