8f274e57f458cb2754d23ef73231aa2faaa32c4c
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-01-23 12:04:38 +0000

    Add cart_item

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -6,16 +6,25 @@ class Customers::CartItemsController < ApplicationController
   end
 
 	def update
+	  @cart_item = CartItem.find(params[:id])
     @cart_item.update(amount: params[:cart_item][:amount].to_i)
-    @price = sub_total(@cart_item).to_s(:delimited)
-    @cart_items = current_cart
-    @total = amount(@cart_items).to_s(:delimited)
-    # redirect_to customers_cart_items_path
+    # @price = sub_total(@cart_item).to_s(:delimited)
+    # @cart_items = current_cart
+    # @total = amount(@cart_items).to_s(:delimited)
+     redirect_to cart_items_path
 	end
 
 	def create
     @cart_item = CartItem.new(params_cart_item)
     @cart_item.customer_id = current_customer.id
+    @cart_items=current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+    if  cart_item.item_id==@cart_item.item_id
+        new_amount = cart_item.amount + @cart_item.amount
+        cart_item.update_attribute(:amount, new_amount)
+        @cart_item.delete
+    end
+  end
     @cart_item.save
     redirect_to cart_items_path
 
@@ -28,14 +37,9 @@ class Customers::CartItemsController < ApplicationController
 	end
 
 	def destroy
+	  @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
-    @cart_items = current_cart
-    @total = total_price(@cart_items).to_s(:delimited)
-    # respond_to do |format|
-    #   format.html{ redirect_to customers_cart_items_path }
-    #   format.js
-    # end
-    # redirect_to customers_cart_items_path
+    redirect_to cart_items_path
 	end
 
   private
