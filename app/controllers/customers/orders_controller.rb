b03ba6da779d80d31cb96821b57aee2e4daa605c
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-01-26 01:53:23 +0000

    Add order

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -9,13 +9,17 @@ class Customers::OrdersController < ApplicationController
   def create
     cart_items = current_customer.cart_items.all
     @order = current_customer.orders.new(order_params)
+    @order.shipping_cost = 800
+    #status = enumで定義したやつを入れてあげる
     if @order.save
+      #binding.pry
       cart_items.each do |cart|
         order_detail = OrderDetail.new
         order_detail.item_id = cart.item_id
         order_detail.order_id = @order.id
         order_detail.amount = cart.amount
         order_detail.price = cart.item.price
+        #making_status = enumで定義したやつを入れてあげる
         order_detail.save
       end
       redirect_to complete_path
@@ -53,18 +57,18 @@ class Customers::OrdersController < ApplicationController
 
   def index
     @orders = current_customer.orders.all
-    @order_details =OrderDetail.all
+    #@order_details =OrderDetail.all
   end
 
   def show
     @order =Order.find(params[:id])
-    @order_details = OrderDetail.all
+    #@order_details = OrderDetail.all
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:name, :address, :total_payment, :payment_method)
+    params.require(:order).permit(:name, :address, :total_payment, :payment_method, :postal_code)
   end
 
   def address_params
